7ab2f6ac044c62b304806440903309e2e8d5ac1f
Logan <loganmsorensen@gmail.com>
2021-02-17 16:19:53 -0800

    sidebar is now rendering data from weather api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import SideBar from "./components/sidebar";
 
 import "./styles/index.css";
 
 function App() {
+  const [location, setLocation] = useState(null);
+
+  const baseUrl = `https://tranquil-bayou-42049.herokuapp.com/`;
+
+  const hitAPI = () => {
+    axios
+      .get(`${baseUrl}https://www.metaweather.com/api/location/44418/`)
+      .then((res) => {
+        console.log(res.data);
+        setLocation(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    hitAPI();
+  }, []);
+
   return (
     <div className="App">
-      <SideBar />
+      <SideBar location={location} />
+      {/* <button onClick={hitAPI}>Hit API</button> */}
     </div>
   );
 }
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -8,37 +8,50 @@ import {
 import ShowerCloud from "../assets/Shower.png";
 import BackgroundImg from "../assets/Cloud-background.png";
 
-const SideBar = () => {
+const SideBar = ({ location }) => {
+//   let { consolidated_weather } = location;
+
+//   console.log(location === {})
+
   return (
-    <div className="sidebar">
-      <div className="location-btns">
-        <button className="search-btn">Search for places</button>
-        <button className="current-loc-btn">
-          <FontAwesomeIcon icon={faLocationArrow} />
-        </button>
-      </div>
-      <div className="weather-icon-cont">
-        <img src={BackgroundImg} alt="" className="background-img" />
-        <img src={ShowerCloud} alt="" className="weather-icon" />
-      </div>
-      <div className="weather-info">
-        <div className="temperature">
-          <p>
-            15<span>°C</span>
-          </p>
+
+        <div className="sidebar">
+      {location !== null && (
+          <>
+          <div className="location-btns">
+            <button className="search-btn">Search for places</button>
+            <button className="current-loc-btn">
+              <FontAwesomeIcon icon={faLocationArrow} />
+            </button>
+          </div>
+          <div className="weather-icon-cont">
+            <img src={BackgroundImg} alt="" className="background-img" />
+            <img src={ShowerCloud} alt="" className="weather-icon" />
+          </div>
+          <div className="weather-info">
+            <div className="temperature">
+              <p>
+                {Math.round(location.consolidated_weather[0].the_temp)}
+                <span>°C</span>
+              </p>
+            </div>
+            <p className="weather-type">
+              {location.consolidated_weather[0].weather_state_name}
+            </p>
+            <div className="date">
+              <span>Today</span>
+              <span>•</span>
+              <span>{location.consolidated_weather[0].applicable_date}</span>
+            </div>
+            <div className="location">
+              <FontAwesomeIcon icon={faMapMarkerAlt} />
+              <span>{location.title}</span>
+            </div>
+          </div>
+          </>
+      )}
         </div>
-        <p className="weather-type">Shower</p>
-        <div className="date">
-          <span>Today</span>
-          <span>•</span>
-          <span>Fri, 5 Jun</span>
-        </div>
-        <div className="location">
-          <FontAwesomeIcon icon={faMapMarkerAlt} />
-          <span>Helsinki</span>
-        </div>
-      </div>
-    </div>
+   
   );
 };
 
