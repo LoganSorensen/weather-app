e56d56a2f6481e663770316dfcfa72cfa5579db2
Logan <loganmsorensen@gmail.com>
2021-02-18 17:58:47 -0800

    weather icon now displays correctly

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -6,14 +6,23 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { connect } from "react-redux";
 
-import ShowerCloud from "../assets/Shower.png";
-import BackgroundImg from "../assets/Cloud-background.png";
+// import Clear from "../assets/Clear.png";
+// import Snow from "../assets/Hail.png";
+// import BackgroundImg from "../assets/Cloud-background.png";
 
 const SideBar = ({ cityName, weather }) => {
   const openSearch = () => {
     let search = document.querySelector(".sidebar-search");
     search.classList.add("sidebar-search--open");
   };
+
+  
+  const setWeatherImg = () => {
+    const weatherName = weather[0].weather_state_name.replace(/ +/g, "")
+    console.log(weatherName)
+    return weatherName
+
+  }
 
   return (
     <div className="sidebar">
@@ -28,8 +37,8 @@ const SideBar = ({ cityName, weather }) => {
             </button>
           </div>
           <div className="weather-icon-cont">
-            <img src={BackgroundImg} alt="" className="background-img" />
-            <img src={ShowerCloud} alt="" className="weather-icon" />
+            <img src='/assets/Cloud-background.png' alt="background clouds" className="background-img" />
+            <img src={`/assets/${setWeatherImg()}.png`} alt="" className="weather-icon" />
           </div>
           <div className="weather-info">
             <div className="temperature">
