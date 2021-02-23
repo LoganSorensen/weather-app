ab7618466b43cd6a1f9bfc2194a0ec227d6641fa
Logan <loganmsorensen@gmail.com>
2021-02-23 00:55:39 -0800

    added geolocation

--- src/App.js
+++ src/App.js
@@ -7,19 +7,40 @@ import SideBarSearch from "./components/sideBarSearch";
 import FiveDayForecast from "./components/fiveDayForecast";
 import Highlights from "./components/highlights";
 
-import { setLocation, toggleCelcius } from "./actions/setLocationActions";
+import {
+  setLocation,
+  toggleCelcius,
+  fetchLocation,
+} from "./actions/setLocationActions";
 import { weatherAPI } from "./utils/weatherAPI";
 
 import "./styles/index.css";
 
 function App(props) {
-  useEffect(() => {
+  const success = (position) => {
+    const latitude = Math.round(position.coords.latitude * 100) / 100;
+    const longitude = Math.round(position.coords.longitude * 100) / 100;
+
+    weatherAPI()
+      .get(`location/search/?lattlong=${latitude},${longitude}`)
+      .then((res) => {
+        props.fetchLocation(res.data[0].woeid);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const error = (error) => {
+    console.log(error);
     weatherAPI()
       .get("location/44418/")
       .then((res) => {
         props.setLocation(res.data);
       })
       .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success, error);
   }, []);
 
   const changeTemp = (e) => {
@@ -75,4 +96,8 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, { setLocation, toggleCelcius })(App);
+export default connect(mapStateToProps, {
+  setLocation,
+  toggleCelcius,
+  fetchLocation,
+})(App);
